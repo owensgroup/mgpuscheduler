a8f371357a1b84bbe1926fbfd44169bb08391348
Collin McCarthy <collin.m.mccarthy@gmail.com>
2016-06-03 19:12:03 -0700

    Modified script, actual running parameters.

--- runExperiments.py
+++ runExperiments.py
@@ -5,29 +5,21 @@ from timeit import default_timer as timer
 
 def main(argv):
 
-   # See https://docs.python.org/2/howto/argparse.html for more details
+   # Example execution:
+   # python runExperiments.py D:\Research\GPUScheduler\build\Release\sched.exe
+   
    parser = argparse.ArgumentParser()
-   parser.add_argument("executable", help="The scheduler executable path")
+   parser.add_argument("executable", help="The scheduler executable path") # See https://docs.python.org/2/howto/argparse.html for more details
    args = parser.parse_args()
-
-   # Debugging experiment values
-   #threadsPerBlockList = [1024]
-   #vectorSizeList = [1024, 16384, 65536]
-   #vectorSizeList = [1024]
-   #matrixSizeList = [32]
-   #maxDevicesList = [1]
-   #batchSizeList = [128]
-   #numRepeat = 1
-   #batchSizeList = [128, 512, 1024]
 
    # Real experiment values
    threadsPerBlockList = [256, 512, 1024]
    blockWidthList = [8, 16, 32] 
-   vectorSizeList = [8192, 16384, 32768, 65536, 131072, 262144] # MUST be same number of elements as matrixSizeList
-   matrixSizeList = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192] # MUST be same number of elements as vectorSizeList
+   vectorSizeList = [32768, 65536, 131072, 262144, 524288, 1048576] # MUST be same number of elements as matrixSizeList
+   matrixSizeList = [128, 256, 512, 1024, 2048, 4096] # MUST be same number of elements as vectorSizeList
    maxDevicesList = [1, 2]
-   batchSizeList = [32, 64, 128, 256, 512, 1024]
-   numRepeat = 3
+   batchSizeList = [8, 16, 32, 64, 128, 256, 512]
+   numRepeat = 1
    verboseFlag = 1 # Use 1 to debug for now, then 0 to run
    
    # "Usage: <ExecPath> inputSize batchSize maxDevices kernelName kernelArgument verboseOutput\n"
@@ -52,7 +44,7 @@ def main(argv):
                blockWidth = blockWidthList[kernelArgumentIndex]
                print('Input: {0}, Batch: {1}, Threads: {2}, GPUs: {3}...'.format(matrixSize, batchSize, blockWidth, maxDevices))
                startCall = timer() 
-               subprocess.call([args.executable, str(matrixSize), str(batchSize), str(maxDevices), 'MatrixMultiply', str(blockWidth), str(verboseFlag)]) 
+               subprocess.call([args.executable, str(matrixSize), str(batchSize), str(maxDevices), 'MatrixMultiply', str(blockWidth), str(numRepeat), str(verboseFlag)]) 
                endCall = timer()
                print('\t... Done, {0}s'.format(endCall-startCall))
 
@@ -61,7 +53,7 @@ def main(argv):
          print('BatchSize {0} finished in {1}s'.format(batchSize, endBatchSize-startBatchSize))
          print('------------------------------------------------')
       endInputSize = timer()
-      print '================================================'
+      print('================================================')
       print('VectorSize {0} and MatrixSize {1} finished in {2}s'.format(vectorSizeList[inputIndex], matrixSizeList[inputIndex], endInputSize-startInputSize))
       print('================================================')
 
